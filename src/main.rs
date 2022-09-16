6d1774921e1372ef5bef5619fdb847173ebac12e
Oyoshi <rafal.apriasz@gmail.com>
2022-09-16 12:57:08 +0200

    feat: implement addition and substraction of two integers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,105 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Clone, Debug, Eq, PartialEq)]
+enum Token {
+    INTEGER(i32),
+    PLUS,
+    MINUS,
+    EOF,
 }
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub struct Interpreter {
+    input: String,
+    current_pos: i32,
+    current_token: Option<Token>,
+}
+
+impl Interpreter {
+    fn new(input: String) -> Interpreter {
+        let mut interpreter = Interpreter {
+            input: input,
+            current_pos: 0,
+            current_token: None,
+        };
+        interpreter
+    }
+
+    fn get_next_token(&mut self) -> Token {
+        if self.current_pos > self.input.len() as i32 - 1 {
+            return Token::EOF
+        }
+
+        let current_char = self.input.as_bytes()[self.current_pos as usize] as char;
+
+        if current_char.is_digit(10) {
+            self.current_pos += 1;
+            return Token::INTEGER(current_char.to_digit(10).unwrap() as i32);
+        }
+
+        if current_char == '+' {
+            self.current_pos += 1;
+            return Token::PLUS;
+        }
+
+        if current_char == '-' {
+            self.current_pos += 1;
+            return Token::MINUS;
+        }
+
+        // panic or error
+        Token::EOF
+
+    }
+
+    fn eat(&mut self, token: Token) {
+        if token == self.current_token.clone().unwrap() {
+            self.current_token = Some(self.get_next_token());
+        }
+        // else error
+    }
+
+    fn expr(&mut self) -> i32 {
+        let mut result: i32 = 0;
+        self.current_token = Some(self.get_next_token());
+        let lhs = self.current_token.clone().unwrap();
+        match lhs {
+            Token::INTEGER(i) => {
+                self.eat(Token::INTEGER(i));
+                result = i;
+            },
+            _ => panic!("Invalid syntax"),
+        }
+        let op = self.current_token.clone().unwrap();
+        if op == Token::PLUS {
+            self.eat(Token::PLUS);
+        }
+        else {
+            self.eat(Token::MINUS);
+        }
+        let rhs = self.current_token.clone().unwrap();
+        match rhs {
+            Token::INTEGER(i) => {
+                self.eat(Token::INTEGER(i));
+                if op == Token::PLUS {
+                    result += i;
+                }
+                else {
+                    result -= i;
+                }
+            },
+            _ => panic!("Invalid syntax"),
+        }
+        return result;
+    }
+}
+
+fn main() {
+    let text = String::from("5-9");
+    let mut interpreter = Interpreter::new(text);
+    println!("5 - 9 = {:?}", interpreter.expr());
 }
 
 #[test]
-fn test_add() {
-    assert_eq!(add(1, 2), 3);
+fn test_interpreter() {
+    let text = String::from("5-9");
+    let mut interpreter = Interpreter::new(text);
+    assert_eq!(interpreter.expr(), -4);
 }
